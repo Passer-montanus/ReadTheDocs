c3aa7adf2bb8f5223a7d02b0615fd77cffd9f948
LittledT <Lxt100@outlook.com>
2025-05-24 15:21:02 +0800

    添加logo和必要extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,15 @@
 # Configuration file for the Sphinx documentation builder.
 
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 # -- Project information -----------------------------------------------------
 
 project = 'ValBal-Trajectory'
@@ -8,8 +18,14 @@ author = 'LittledT'
 release = 'v0.0.1'
 
 # -- General configuration ---------------------------------------------------
+master_doc = 'index'
 
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autodoc.typehints',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
     'myst_parser',
     "sphinxcontrib.mermaid",
     "sphinx_copybutton",
@@ -37,20 +53,20 @@ language = 'zh_CN'
 
 html_theme = 'sphinx_rtd_theme'
 
-html_theme_options = {
-    'analytics_anonymize_ip': False,
-    'logo_only': True,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False,
-}
+# html_theme_options = {
+#     'analytics_anonymize_ip': False,
+#     'logo_only': True,
+#     'display_version': True,
+#     'prev_next_buttons_location': 'bottom',
+#     'style_external_links': False,
+#     'collapse_navigation': True,
+#     'sticky_navigation': True,
+#     'navigation_depth': 4,
+#     'includehidden': True,
+#     'titles_only': False,
+# }
 
-# html_logo = "./_static/logo.png"
+html_logo = 'imgs/Logo/GIPP.png'
 html_static_path = ['_static']
 # html_js_files = [
 #     'my_custom.js',
